a101e0a85a404f4087e41fec75433f54dddaea7f
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-06-17 15:52:28 -0700

    Added Math and Comparision Operator Assignment

---
+++ MathandComparisonOperator/MathandComparisonOperator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace MathandComparisonOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Please enter your hourly rate.");
+            // Created string variable for input
+            string hourlyRate = Console.ReadLine();
+            // Converting to decimal since we are working with money
+            decimal hourly = Convert.ToDecimal(hourlyRate);
+            Console.WriteLine("Please enter hours worked per week.");
+            string hoursWorked = Console.ReadLine();
+            // Converting to decimal to work with hourly and hours per week expression
+            decimal hoursWorkedprweek = Convert.ToDecimal(hoursWorked);
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Please enter your hourly rate.");
+            string hourlyRate2 = Console.ReadLine();
+            decimal hourly2 = Convert.ToDecimal(hourlyRate2);
+            Console.WriteLine("Please enter hours worked per week.");
+            string hoursWorked2 = Console.ReadLine();
+            decimal hoursWorkedprweek2 = Convert.ToDecimal(hoursWorked2);
+            decimal salary1 = hourly * hoursWorkedprweek * 52;
+            decimal salary2 = hourly2 * hoursWorkedprweek2 * 52;
+            Console.WriteLine("Annual salary of Person 1: " + salary1);
+            Console.WriteLine("Annual salary of Person 2: " + salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(salary1 > salary2);
+            Console.ReadLine();
+        }
+    }
+}
