5739e3bc0cb54ebcc549236861f97c6d13f8afb2
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-06-27 13:40:09 -0700

    Added while loop

--- BooleanComparionWhile/BooleanComparionWhile/Program.cs
+++ BooleanComparionWhile/BooleanComparionWhile/Program.cs
@@ -15,36 +15,55 @@ namespace BooleanComparionWhile
             // Input is converted from string to an int
             int favoriteDay = Convert.ToInt32(Console.ReadLine());
             bool goodGuess = favoriteDay == 1;
-            switch (favoriteDay)
-            // the value of the expression is compared with the value of each case.
-            // if there is a match, the associated block of code is executed.
+            while(!goodGuess)
             {
-                // if the variable value is case X then execute block
-                case 1:
-                    Console.WriteLine("Monday! Monday, is my favorite day of the week!");
-                    // When the code reaches the break, it will break out of this entire code block.
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday? Uh Uh Uh, you didn't say the magic word.");
-                    break;
-                default:
-                    Console.WriteLine("Please pick a day out of the week.");
-                    break;
+                switch (favoriteDay)
+                // the value of the expression is compared with the value of each case.
+                // if there is a match, the associated block of code is executed.
+                {
+                    // if the variable value is case X then execute block
+                    case 1:
+                        Console.WriteLine("Monday! Monday, is my favorite day of the week!");
+                        goodGuess = true;
+                        // When the code reaches the break, it will break out of this entire code block.
+                        break;
+                    case 2:
+                        Console.WriteLine("Tuesday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("Wednesday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("Thursday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.WriteLine("Friday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("Saturday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("Sunday? Uh Uh Uh, you didn't say the magic word.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Please pick a day out of the week.");
+                        Console.WriteLine("Guess again.");
+                        favoriteDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
             }
 
             Console.WriteLine("Please guess my daughters age from 1-7?");
@@ -100,7 +119,7 @@ namespace BooleanComparionWhile
                 }
             }
             while (!kidsAge);
-            Console.ReadLine();
+
             Console.ReadLine();
         }
     }
