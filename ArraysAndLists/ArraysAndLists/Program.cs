e7e64a5492b8cf6ea0c102affbc00735207c83d8
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-06-29 12:29:48 -0700

    Added list

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -9,24 +9,53 @@ namespace MyApplication
         {
 
             // Created a string array with pizza toppings
-            string[] pizzaToppings = { "Pepperoni", "Cheese", "Olives", "Sausage", "Peanut Butter?" };
+            //string[] pizzaToppings = { "Pepperoni", "Cheese", "Olives", "Sausage", "Peanut Butter?" };
 
-            Console.WriteLine("I have prepared a pizza topping list");
-            Console.WriteLine("Please enter a number 0-4");
-            int pizzaNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("I have prepared a pizza topping list");
+            //Console.WriteLine("Please enter a number from 0-4");
+            //int pizzaNum = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Your number chose... " + pizzaToppings[pizzaNum]);
+            //// If input is greater than 4 the user will be asked to enter in a number from 0-4
+            //if (pizzaNum <= 4)
+            //{
+            //    // Passing input value to array 
+            //    Console.WriteLine("The topping you chose is " + pizzaToppings[pizzaNum]);
+            //}
+            //else
+            //{
+            //    // This will let user know input was not in range of index.
+            //    Console.WriteLine(pizzaNum + " index does not exist.");
+            //}
+
+            //// Created an array of integers
+            //int[] numbers = { 15, 12, 65, 76, 42, 53 };
+            //Console.WriteLine("Please enter a number from 0-5 to pick from an array");
+            //// Converting input string to int variable
+            //int numberGuess = Convert.ToInt32(Console.ReadLine());
+
+            //if (numberGuess <= 5)
+            //{
+            //    Console.WriteLine("The number in the array you chose... " + numbers[numberGuess]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(numberGuess + " index does not exist.");
+            //}
+
+            // Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
+
+            List<string> listOfStrings = new List<string>();
+            listOfStrings.Add("Person");
+            listOfStrings.Add("Place");
+            listOfStrings.Add("Thing");
+
+            foreach (string item in listOfStrings)
+            {
+                Console.WriteLine(item);
+            }            
 
 
-            // Created an array of integers
-            int[] numbers = { 15, 12, 65, 76, 42, 53 };
-            Console.WriteLine("Please enter a number from 0-5 to pick from an array");
-            // Converting input string to int variable
-            int numberGuess = Convert.ToInt32(Console.ReadLine());
-            // Passing input value to array 
-            Console.WriteLine("Your number chose... " + numbers[numberGuess]);
-
-            Console.ReadLine();
+            }
 
         }
     }
