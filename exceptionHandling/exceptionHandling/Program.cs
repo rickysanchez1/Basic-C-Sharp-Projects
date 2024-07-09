3bf6405b88ecefdc81a360c9c23ce0107299cecd
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-07-09 10:36:19 -0700

    Added exception handling exercise with user input

--- exceptionHandling/exceptionHandling/Program.cs
+++ exceptionHandling/exceptionHandling/Program.cs
@@ -15,67 +15,45 @@ namespace exceptionHandling
 
             // Ask the user for a number to divide each number in the list by.
             Console.WriteLine("Please enter a number and I will divide it by each number on the list.");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
-
+            
+            // try catch loop to handle exceptions
             try
             {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
                 // a loop that divides each integer in the list by the number the user entered.
                 for (int i = 0; i < numbers.Count; i++)
                 {
-                    int userDivide = userNumber / numbers[i];
-                    Console.WriteLine(userNumber + " / " + numbers[i] + " = " + userDivide);
+                    // expression for the userNumber and int in list
+                    int userDivide = numbers[i] / userNumber;
+                    // Displays the result of userNumber against each int in list
+                    Console.WriteLine(numbers[i] + " / " + userNumber + " = " + userDivide);
                 }
             }
             catch (FormatException ex)
             {
-                Console.WriteLine("Please enter whole number.");
+                // if string was entered, display message
+                Console.WriteLine("Please type whole number");
+                return;
             }
+            // exception handing using dividebyzero
             catch (DivideByZeroException ex)
             {
                 Console.WriteLine("Please do not divide by zero.");
+                return;
             }
-            // exception using customer 
-
-            // a loop that divides each integer in the list by the number the user entered.
-
-
-
-            // displays the result to the screen.
-
-            //try
-            //{
-            //    Console.WriteLine("Pick a number.");
-            //    int numberOne = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine("Pick a second number.");
-            //    int numberTwo = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine("Divide the two...");
-            //    int numberThree = numberOne / numberTwo;
-            //    Console.WriteLine(numberOne + " Divided by " + numberTwo + " equals " + numberThree);
-            //    Console.ReadLine();
-            //}
-            //// catch (Format exception variable
-            //catch (FormatException ex)
-            //{
-            //    // if exception found execute code
-            //    Console.WriteLine("Please type whole number");
-            //    return;
-            //}
-            //// exception handing using dividebyzero
-            //catch (DivideByZeroException ex)
-            //{
-            //    Console.WriteLine("Please do not divide by zero.");
-            //}
-            //// exception using customer 
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
-            //finally
-            //{
-            //    Console.ReadLine();
-            //}
-            Console.ReadLine();
            
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+
+
+
+
+           
+
         }
     }
 }
