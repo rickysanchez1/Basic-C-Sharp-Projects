4b00e28b91e2f8743f6e42579fd3fef0f80a2dd3
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-07-29 20:44:02 -0700

    Added Main Method assignment. It utilizes 1 method name for 3 different parameters

---
+++ MainMethod/MainMethod/MathOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MathOperation
+    {
+        // Method that will take in an integer
+        public int returnInt(int number)
+        {
+            // Create a Math operation with an int parameter
+            int returnValue = number + 50;
+            // return integer
+            return returnValue;
+        }
+
+        public int returnInt(decimal number)
+        {
+            // convert decimal to int and multiplied by 10
+            int returnValue = decimal.ToInt32(number) * 10;
+
+            return returnValue;
+        }
+        public int returnInt(string number)
+        {
+            // Convert int to string
+            int returnValue = Int32.Parse(number) / 5;
+            // return string as an int
+            return returnValue;
+        }
+
+
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating class method
+            MathOperation number = new MathOperation();
+
+            Console.WriteLine("Please enter a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            // Calling method and passing user input as a parameter
+            int result = number.returnInt(num);
+            // Display result as an integer
+
+            Console.WriteLine("Your number + 50 = " + result);
+
+            // Asking user to enter a number with a decimal
+            Console.WriteLine("Enter a number with a decimal.");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            int resultDecimal = number.returnInt(num2);
+            // Display result as an integer
+            Console.WriteLine("Your decimal number multiplied by 10, rounded up = " + resultDecimal);
+
+            // Asking user to enter a number
+            Console.WriteLine("Please enter in another number.");
+            string stringNumber = Console.ReadLine();
+            int resultString = number.returnInt(stringNumber);
+            // Display result as an integer
+            Console.WriteLine("Your number divided by = 20" + resultString);
+
+            Console.ReadLine();
+        }
+    }
+}
