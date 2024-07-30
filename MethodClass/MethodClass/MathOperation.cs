a8a9d6a2ce28bd5882d1f94718ab30dcfa006b4f
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-07-30 15:39:33 -0700

    Added Method Class assignment utilizing calling methods with variables and with numbers

---
+++ MethodClass/MethodClass/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class MathOperation
+    {
+        // declaring void method that takes in 2 int variables.
+        public void twoIntegers(int a, int b)
+        {
+            // Assign math operation to first variable
+            int sum = a + 10;
+            // Display second variable
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating class method
+            MathOperation number = new MathOperation();
+
+            Console.WriteLine("Please enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Calling method and passing two values
+            number.twoIntegers(5, 10);
+
+            // Calling method passing 2 variables
+            number.twoIntegers(num, num2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
