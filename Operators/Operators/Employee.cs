0735c3d1fba135d5fd62b09c425fe92968c5346c
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-06 15:54:39 -0700

    Added Operators assignment with overloaded operator method

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+     class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloaded method comparison with Employee class and Id property
+        public static bool operator ==(Employee name1, Employee name2)
+        {
+            return name1.Id == name2.Id;
+        }
+
+        public static bool operator !=(Employee name1, Employee name2)
+        {
+            return !(name1.Id == name2.Id);
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating 2 objects with values to their properties
+            Employee name1 = new Employee { 
+                Id = 1, 
+                FirstName = "Jane", 
+                LastName = "Jones" 
+            };
+            Employee name2 = new Employee { 
+                Id = 1, 
+                FirstName = "John", 
+                LastName = "Smith" 
+            };
+            // Then compare the two Employee objects using the newly overloaded operators and display the results.
+
+            
+            bool sameId = name1.Id == name2.Id;
+            bool notSameId = name1.Id != name2.Id;
+
+
+            Console.WriteLine(sameId);
+            Console.WriteLine(notSameId);
+            Console.ReadLine();
+        }
+    }
+}
