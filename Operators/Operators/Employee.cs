a16b78b9429c04fb2c7731977bb3d4d653657fed
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-06 18:24:55 -0700

    Added override equals and override gethashcode method

--- Operators/Operators/Employee.cs
+++ Operators/Operators/Employee.cs
@@ -6,21 +6,35 @@ using System.Threading.Tasks;
 
 namespace Operators
 {
-     class Employee
+     public class Employee
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
         // Overloaded method comparison with Employee class and Id property
-        public static bool operator ==(Employee name1, Employee name2)
+        public static bool operator == (Employee name1, Employee name2)
         {
             return name1.Id == name2.Id;
         }
-
-        public static bool operator !=(Employee name1, Employee name2)
+       
+        
+        public static bool operator != (Employee name1, Employee name2)
         {
             return !(name1.Id == name2.Id);
         }
+      
+
     }
 }
