c5585c305ae80b37934e84daa39596bbb0c129c8
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-07 13:43:06 -0700

    Created foreach loop using generics

---
+++ Parameters/Parameters/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    // Created generic type parameter
+    public class Employee<T> 
+    {
+        // Generic list named things
+        public List<T> things { get; set; }
+
+        // Using method to create a new list for things
+        public Employee()
+        {
+            things = new List<T>();
+        }
+    }
+}
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Instantiating stringList object
+            Employee<string> stringList = new Employee<string>();
+            // Adding values through string and creating new list
+            stringList.things.AddRange(new List<string> { "Fred", "Joe", "Bill" });
+
+            // Loop through each name and print to console.
+            foreach (var thing in stringList.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            // Instantiating intList
+            Employee<int> intList = new Employee<int>();
+            // Assigning int values to intList
+            intList.things.AddRange(new List<int> { 1, 2, 3 });
+            
+            foreach (var thing in intList.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
