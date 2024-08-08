65e507c0e4a1fa2fb2afe04cc2bea1f61a50b7e0
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-08 00:16:20 -0700

    Created object with values and utilized struct to set and get

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object of data type Number and assign a value
+            Number number = new Number { Amount = 59 };
+            
+            // Print number and the value that was set through Amount
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+            
+
+        }
+        
+    }
+}
