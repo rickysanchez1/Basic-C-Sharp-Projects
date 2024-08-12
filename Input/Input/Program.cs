b6289f550074ee7dcb2ebbafc69c783b08d588b5
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-12 14:45:21 -0700

    Added Input Assignment utilizing System.IO to ask for number, write to file and return file

---
+++ Input/Input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string userInput = number.ToString();
+            string fileName = @"C:\Users\sinp3\OneDrive\Documents\GitHub\Basic-C-Sharp-Projects\twentyone\log.txt";
+            File.WriteAllText(fileName, userInput);
+            
+            string readAloud = File.ReadAllText(fileName);
+           
+            Console.WriteLine(readAloud);
+            Console.ReadLine();
+        }
+    }
+}
