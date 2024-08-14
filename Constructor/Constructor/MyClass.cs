cdfc0ffe5b07331eea956d163e113bbc0342add5
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-08-14 13:34:11 -0700

    Added Constuctor Assignment utilitzing declaration of variables using var and chaining constructors

---
+++ Constructor/Constructor/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    public class MyClass
+    {
+        // Chaining constructor to provide default value is none is given
+        public MyClass(string name) : this(name, 500)
+        {
+
+        }
+        public MyClass(string name, int money)
+        {
+            Console.WriteLine(name + money);
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating const var
+            const string person = "person";
+            var money = 100;
+
+            MyClass myClass = new MyClass(person);
+
+
+            Console.ReadLine();
+        }
+    }
+}
