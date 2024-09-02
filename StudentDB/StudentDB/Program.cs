4474caebfdbdd2ff1e0702db6bd0fa11ba662396
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-09-02 14:30:24 -0700

    Created a console app to add 1 student to the db

---
+++ StudentDB/StudentDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jeffree" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
